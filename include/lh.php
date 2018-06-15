e244fcc8e6096e68d6581554cd223851aeaa9b5a
Kim - DG9VH <dg9vh@users.noreply.github.com>
2018-06-15 13:46:53 +0200

    Update lh.php

--- include/lh.php
+++ include/lh.php
@@ -24,13 +24,13 @@ for ($i = 0; $i < count($lastHeard); $i++) {
 		if (constant("SHOWQRZ") && $listElem[1] !== "??????????" && !is_numeric($listElem[1])) {
 			echo"<td nowrap><a target=\"_new\" href=\"https://qrz.com/db/$listElem[1]\">".str_replace("0","&Oslash;",$listElem[1])."</a></td>";
 		} else {
-			if (constant("GDPR"))
+			if (defined("GDPR"))
 				echo"<td nowrap>".str_replace("0","&Oslash;",substr($listElem[1],0,3)."***")."</td>";
 			else
 				echo"<td nowrap>".str_replace("0","&Oslash;",$listElem[1])."</td>";
 		}
 		echo"<td>$listElem[2]</td>";
-		if (constant("GDPR"))
+		if (defined("GDPR"))
 			echo"<td nowrap>".str_replace("0","&Oslash;",substr($listElem[3],0,3)."***")."</td>";
 		else
 			echo"<td nowrap>".str_replace("0","&Oslash;",$listElem[3])."</td>";
